031154abe9871e6f93dd1c1734a23dba2c4f25eb
NathanLuan <nathanuluan@gmail.com>
2024-12-16 15:44:24 -0500

    Update Warrior.java

--- Warrior.java
+++ Warrior.java
@@ -1,11 +1,11 @@
 public class Warrior extends Adventurer{
     private int rage;
- 
-    
-     
+
+
+
     public Warrior(String name, int hp, int attackDMG, int rage){
         super(name, hp, attackDMG);
-        
+
         this.rage = rage;
     }
     
@@ -19,21 +19,21 @@ public class Warrior extends Adventurer{
     public void setSpecial(int n){
         this.rage = n;
     }
- 
-    
+
+
   /*
     all adventurers must have a way to attack enemies and
     support their allys
   */
       //hurt or hinder the target adventurer
       public String attack(Adventurer other){
-    
+
         other.applyDamage(this.getAttack());
         if(other.isAlive()){
             this.applyDamage(other.getAttack());
         }
         return "Your health: " + this.getHP() + "Opp's health: " + other.getHP();
-         
+
       }
 
       //hurt or hinder the target adventurer, consume some special resource
@@ -62,5 +62,5 @@ public class Warrior extends Adventurer{
     public int getSpecialMax(){
         return 0;
     }
-    
+
 }
