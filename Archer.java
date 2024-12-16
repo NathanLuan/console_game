57e8e7d7b035d50c433cd0df074d8c011507a4b6
NathanLuan <nathanuluan@gmail.com>
2024-12-16 15:42:00 -0500

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -1,14 +1,14 @@
 public class Archer extends Adventurer{
     private int snipe;
- 
-    
-     
+
+
+
     public Archer(String name, int hp, int attackDMG, int snipe){
         super(name, hp, attackDMG);
-        
+
         this.snipe = snipe;
     }
-    
+
     public String getSpecialName(){
         return "snipe";
     }
@@ -19,21 +19,21 @@ public class Archer extends Adventurer{
     public void setSpecial(int n){
         this.snipe = n;
     }
- 
-    
+
+
   /*
     all adventurers must have a way to attack enemies and
     support their allys
   */
       //hurt or hinder the target adventurer
       public String attack(Adventurer other){
-    
+
         other.applyDamage(this.getAttack());
         if(other.isAlive()){
             this.applyDamage(other.getAttack());
         }
         return "Your health: " + this.getHP() + "Opp's health: " + other.getHP();
-         
+
       }
 
       //hurt or hinder the target adventurer, consume some special resource
@@ -62,5 +62,5 @@ public class Archer extends Adventurer{
     public int getSpecialMax(){
         return 0;
     }
-    
+
 }
